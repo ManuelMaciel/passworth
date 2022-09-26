6df9319e59bff75e3c081cd13c12f3c38c2e2711
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-09-26 16:26:40 -0400

    final commit

---
+++ src/component/TheLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex cursor-pointer" @click="SET_LABEL(null)">
+    <button
+        type="button"
+        class="border-none bg-none uppercase text-xs px-2 py-1 text-gray-400 hover:opacity-70"
+      >
+       Passworth
+      </button>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from "../store";
+
+const store = useStore();
+const { SET_LABEL } = store;
+</script>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import pinia from "./createPinia";
+import Notifications from "@kyvg/vue3-notification";
+import mdiVue from "mdi-vue/v3";
+import * as mdijs from "@mdi/js";
+import i18n from "./i18n";
+import App from "./App.vue";
+import "./style/index.css";
+
+createApp(App)
+  .use(pinia)
+  .use(i18n)
+  .use(Notifications)
+  .use(mdiVue, {
+    icons: mdijs,
+  })
+  .mount("#app");
